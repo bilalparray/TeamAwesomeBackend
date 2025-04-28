e47e7ca4737832b89960cb11fb433c807dfaf20f
Bilal Ahmad <dev.renosoftwares@gmail.com>
2025-04-28 21:54:23 +0530

    keyob url update

--- public/appconstants.js
+++ public/appconstants.js
@@ -1,4 +1,5 @@
 export const AppConstants = {
-  baseUrl: "https://teamawesomebackend-sgsc.onrender.com",
+  // baseUrl: "https://teamawesomebackend-sgsc.onrender.com",
+  baseUrl: "https://environmental-antonetta-bilalparray-82a778b4.koyeb.app",
   // baseUrl: "http://localhost:3000",
 };
