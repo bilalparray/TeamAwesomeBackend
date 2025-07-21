8362d31b484e9c1c2faa44742a4423b2cdc9952b
Bilal Ahmad <dev.renosoftwares@gmail.com>
2025-07-21 10:12:59 +0530

    data backed again with removed career object which came from by updateing data by compass query

--- public/appconstants.js
+++ public/appconstants.js
@@ -1,5 +1,5 @@
 export const AppConstants = {
   // baseUrl: "https://teamawesomebackend-sgsc.onrender.com",
-  // baseUrl: "https://environmental-antonetta-bilalparray-82a778b4.koyeb.app",
-  baseUrl: "http://localhost:3000",
+  baseUrl: "https://environmental-antonetta-bilalparray-82a778b4.koyeb.app",
+  // baseUrl: "http://localhost:3000",
 };
